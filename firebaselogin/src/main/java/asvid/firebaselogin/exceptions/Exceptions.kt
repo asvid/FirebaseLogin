3eed4a6f725cea0758d3ce9832ad6a82c15355c7
adam <adam.swiderski89@gmail.com>
2017-04-24 00:15:35 +0200

    fixed compile error

--- firebaselogin/src/main/java/asvid/firebaselogin/exceptions/Exceptions.kt
+++
@@ -1,6 +0,0 @@
-package asvid.firebaselogin.exceptions
-
-class NotInitializedException : Throwable("UserLoginService was not initialized before call")
-class WrongPassword : Throwable("Provided password is wrong")
-class EmailAlreadyUsed : Throwable("Email already used")
-class WeakPassword : Throwable("Password is too weak")
